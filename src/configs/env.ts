48cd95a2dd8797f3d73dad6d9bc57c514504a113
AkkalDhami <dhamiakkal21@gmail.com>
2025-11-15 09:05:27 +0545

    configure db and .env

---
+++ src/configs/env.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  MONGODB_URI: z.url(),
+
+  JWT_REFRESH_SECRET: z.string(),
+  JWT_ACCESS_SECRET: z.string(),
+
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.string(),
+  SMTP_USER: z.string(),
+  EMAIL_FROM: z.email(),
+
+  CLOUDINARY_CLOUD_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_API_SECRET: z.string(),
+});
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success) {
+  console.error(
+    "❌ Invalid environment variables:",
+    z.flattenError(parsed.error).fieldErrors
+  );
+  process.exit(1);
+}
+
+export const env = parsed.data;
