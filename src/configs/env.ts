79701ffa7aeac689b5f977a1e3c12a6a00a343b4
AkkalDhami <dhamiakkal21@gmail.com>
2025-11-27 08:24:40 +0545

    Update env.ts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -6,6 +6,11 @@ const envSchema = z.object({
     .default("development"),
   MONGODB_URI: z.url(),
 
+  LOG_LEVEL: z
+    .enum(["silent", "error", "warn", "info", "debug", "trace"])
+    .default(process.env.NODE_ENV === "production" ? "info" : "debug"),
+  NEXTAUTH_SECRET: z.string(),
+
   JWT_REFRESH_SECRET: z.string(),
   JWT_ACCESS_SECRET: z.string(),
 
@@ -17,6 +22,9 @@ const envSchema = z.object({
   CLOUDINARY_CLOUD_NAME: z.string(),
   CLOUDINARY_API_KEY: z.string(),
   CLOUDINARY_API_SECRET: z.string(),
+
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 const parsed = envSchema.safeParse(process.env);
